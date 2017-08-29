01287c49751c00d401dcc3e47847547d94aacbd0
Patrik Kovacs <patrik.kovacs@catalysts.cc>
2017-08-29 16:16:35 +0200

    adding example codes

---
+++ examples/ReadingSData.cpp
@@ -0,0 +1,57 @@
+/// \copyright Copyright (c) Catalysts GmbH
+/// \author Patrik Kovacs, Catalysts GmbH
+
+#include <hdf4cpp/hdf.h>
+#include <iostream>
+
+using namespace hdf4cpp;
+
+int main() {
+    // Open the file
+    HdfFile file(TEST_DATA_PATH "small_test.hdf");
+    // Get the item
+    HdfItem item = file.get("DataWithAttributes");
+    // Check the type
+    if (item.getType() == SDATA) {
+        std::cout << "Yes, the type is SData\n";
+    } else {
+        std::cerr << "Type missmatch\n";
+        return 1;
+    }
+
+    // Read the whole data
+    std::vector<float32> vec;
+    item.read(vec);
+    std::cout << "The whole data: ";
+    for (const auto &value : vec) {
+        std::cout << value << ' ';
+    }
+    std::cout << std::endl;
+
+    // Read in a range
+    std::vector<Range> ranges;
+    std::vector<int32> dims = item.getDims();
+    for (const auto &dim : dims) {
+        if (dim >= 1) {
+            ranges.push_back(Range(0, dim - 1));
+        } else {
+            ranges.push_back(Range(0, dim));
+        }
+    }
+    item.read(vec, ranges);
+    std::cout << "Data in the range [0, dim-1) if it's possible, else [0, dim): ";
+    for (const auto &value : vec) {
+        std::cout << value << ' ';
+    }
+    std::cout << std::endl;
+
+    // Read an attribute
+    HdfAttribute attribute = item.getAttribute("Integers");
+    std::vector<int32> integers;
+    attribute.get(integers);
+    std::cout << "The 'Integers' attribute data: ";
+    for (const auto &integer : integers) {
+        std::cout << integer << ' ';
+    }
+    std::cout << std::endl;
+}
---
+++ examples/ReadingVData.cpp
@@ -0,0 +1,46 @@
+/// \copyright Copyright (c) Catalysts GmbH
+/// \author Patrik Kovacs, Catalysts GmbH
+
+#include <hdf4cpp/hdf.h>
+#include <iostream>
+
+using namespace hdf4cpp;
+
+int main() {
+    // Open the file
+    HdfFile file(TEST_DATA_PATH "small_test.hdf");
+    // Get the item
+    HdfItem item = file.get("Vdata");
+    // Reading scalar field
+    {
+        std::vector<int32> vec;
+        item.read(vec, "age");
+        std::cout << "Age field values: ";
+        for (const auto &value : vec) {
+            std::cout << value << ' ';
+        }
+        std::cout << std::endl;
+    }
+    // Reading array field (string)
+    {
+        std::vector<std::vector<char> > vec;
+        item.read(vec, "name");
+        std::cout << "Name field values: ";
+        for(const auto& char_vec : vec) {
+            std::string string(char_vec.data());
+            std::cout << '\'' << string << "' ";
+        }
+        std::cout << std::endl;
+    }
+    // Reading attribute
+    {
+        HdfAttribute attribute = item.getAttribute("attribute");
+        std::vector<int32> vec;
+        attribute.get(vec);
+        std::cout << "Attribute values: ";
+        for(const auto& value : vec) {
+            std::cout << value << ' ';
+        }
+        std::cout << std::endl;
+    }
+}
